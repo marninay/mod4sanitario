31970768c77aac5ce590016daf7636db84d2ad8e
mmgutierrezl <mmgutierrezl@soe.uagrm.edu.bo>
2025-03-01 02:20:20 -0800

    Actualizacion Marlene

---
+++ ProyHospital/Schema/Paciente.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE Paciente (
+    DNI VARCHAR(10) PRIMARY KEY,
+    apellidosnombre VARCHAR(100) NOT NULL,
+    fechaNacimiento DATE,
+    numSS VARCHAR(20) UNIQUE NOT NULL,
+    datosOpcionales TEXT
+);
+
