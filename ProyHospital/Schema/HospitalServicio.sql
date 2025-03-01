ecb27450051f23eb4f7afadc4686987b6cb81d63
marninay <mrninay@soe.uagrm.edu.bo>
2025-02-28 22:31:13 -0800

    Actualizando Tablas

---
+++ ProyHospital/Schema/HospitalServicio.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE HospitalServicio (
+    codHospital INT,
+    idServicio VARCHAR(10),
+    numCamas INT,
+    PRIMARY KEY (codHospital, idServicio),
+    FOREIGN KEY (codHospital) REFERENCES Hospital(codHospital)
+   ON DELETE CASCADE
+    ON UPDATE CASCADE
+,
+    FOREIGN KEY (idServicio) REFERENCES Servicio(idServicio)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+);
+
---
+++ ProyHospital/Schema/Servicio.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE Servicio (
+    idServicio VARCHAR(10) PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    comentario TEXT
+);
