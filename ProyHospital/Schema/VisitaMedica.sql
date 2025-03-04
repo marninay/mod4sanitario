d0531dfe63efb3d82ad45710be7ff849cbeed8cf
ajpacos <ajpacos@soe.uagrm.edu.bo>
2025-03-03 20:56:23 -0400

    Tabla Visita Medica

---
+++ ProyHospital/Schema/VisitaMedica.sql
@@ -0,0 +1,14 @@
+CREATE TABLE VisitaMedica (
+    idVisita INT PRIMARY KEY,
+    fechahora DATETIME NOT NULL,
+    codHospital INT,
+    idServicio VARCHAR(10),
+    DNIMedico VARCHAR(10),
+    codHist INT,
+    diagnostico TEXT,
+    tratamiento TEXT,
+    FOREIGN KEY (codHospital) REFERENCES Hospital(codHospital),
+    FOREIGN KEY (idServicio) REFERENCES Servicio(idServicio),
+    FOREIGN KEY (DNIMedico) REFERENCES Medico(DNI),
+    FOREIGN KEY (codHist) REFERENCES HistoriaClinica(codHist)
+);
