200e8eb671655a72638ad84567c833691723368e
ajpacos <ajpacos@soe.uagrm.edu.bo>
2025-03-03 20:55:52 -0400

    Tabla Internacion

---
+++ ProyHospital/Schema/Internacion.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Internacion (
+    idinternacion INT PRIMARY KEY,
+    idVisita INT,
+    numHabitacion INT,
+    fechaSalida DATE,
+    FOREIGN KEY (idVisita) REFERENCES VisitaMedica(idVisita)
+);
