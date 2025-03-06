031b008bf59f056d401d9f8ba7859c25d23aefee
marninay <mrninay@soe.uagrm.edu.bo>
2025-03-05 19:54:35 -0800

    Proyecto DW_Hospital

---
+++ ProyHospitalDW/Schema/Tables/Dim_Hospital.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE Dim_Hospital (
+    codHospital INT PRIMARY KEY,
+    nombre VARCHAR(100),
+    ciudad VARCHAR(50),
+    telefono VARCHAR(15),
+    director VARCHAR(100)
+);
+
---
+++ ProyHospitalDW/Schema/Tables/Dim_Paciente.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE Dim_Paciente (
+    codHist INT PRIMARY KEY,
+    DNI VARCHAR(10),
+    nombre VARCHAR(100),
+    fechaNacimiento DATE,
+    numSeguridadSocial VARCHAR(20)
+);
+
