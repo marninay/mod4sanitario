bac301df6de79cd33b0c0f514d355872acef4210
mmgutierrezl <mmgutierrezl@soe.uagrm.edu.bo>
2025-03-05 22:21:03 -0800

    Actualizacion Tiempo_Servicio

---
+++ ProyHospitalDW/Schema/Tables/Dim_Servicio.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE Dim_Servicio (
+    idServicio VARCHAR(10) PRIMARY KEY,
+    nombreServicio VARCHAR(100),
+    descripcion TEXT
+);
+
---
+++ ProyHospitalDW/Schema/Tables/Dim_Tiempo.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE Dim_Tiempo (
+    idTiempo INT PRIMARY KEY,
+    fecha DATE,
+    diaSemana VARCHAR(15),
+    mes INT,
+    anio INT
+);
+
